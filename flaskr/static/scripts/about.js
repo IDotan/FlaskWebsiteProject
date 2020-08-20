174586c78ecfca5e697c6829b853a3e8f50c4e2e
BlackTiger145 <62836633+BlackTiger145@users.noreply.github.com>
2020-08-20 13:35:33 +0300

    about.html css and switch tabs change

--- flaskr/static/scripts/about.js
+++ flaskr/static/scripts/about.js
@@ -1,12 +1,16 @@
 function switchTab(tab) {
     if (tab == 'site') {
         document.getElementsByClassName('tab-content')[0].style.backgroundColor = 'rgba(193, 193, 193, 0.7)';
-        document.getElementsByClassName('site-about')[0].classList.add('active')
-        document.getElementsByClassName('dev-about')[0].classList.remove('active')
+        document.getElementsByClassName('site-about')[0].classList.add('active');
+        document.getElementsByClassName('dev-about')[0].classList.remove('active');
+        document.getElementsByClassName('content-site')[0].hidden = false;
+        document.getElementsByClassName('content-dev')[0].hidden = true;
     } else {
         document.getElementsByClassName('tab-content')[0].style.backgroundColor = 'rgba(76, 152, 175, 0.7)';
-        document.getElementsByClassName('site-about')[0].classList.remove('active')
-        document.getElementsByClassName('dev-about')[0].classList.add('active')
+        document.getElementsByClassName('site-about')[0].classList.remove('active');
+        document.getElementsByClassName('dev-about')[0].classList.add('active');
+        document.getElementsByClassName('content-site')[0].hidden = true;
+        document.getElementsByClassName('content-dev')[0].hidden = false;
     }
 }
 
