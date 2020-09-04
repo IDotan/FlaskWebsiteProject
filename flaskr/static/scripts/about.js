ed729adb80ac9da9eb0a9487e9dacb2adb8a7a77
BlackTiger145 <62836633+BlackTiger145@users.noreply.github.com>
2020-09-04 14:09:33 +0300

    about.html add about dev and linkedin script alert when needed

--- flaskr/static/scripts/about.js
+++ flaskr/static/scripts/about.js
@@ -5,12 +5,14 @@ function switchTab(tab) {
         document.getElementsByClassName('dev-about')[0].classList.remove('active');
         document.getElementsByClassName('content-site')[0].hidden = false;
         document.getElementsByClassName('content-dev')[0].hidden = true;
+        document.getElementsByClassName('content-dev')[0].style.display = "none"
     } else {
         document.getElementsByClassName('tab-content')[0].style.backgroundColor = 'rgba(76, 152, 175, 0.7)';
         document.getElementsByClassName('site-about')[0].classList.remove('active');
         document.getElementsByClassName('dev-about')[0].classList.add('active');
         document.getElementsByClassName('content-site')[0].hidden = true;
         document.getElementsByClassName('content-dev')[0].hidden = false;
+        document.getElementsByClassName('content-dev')[0].style.display = "grid"
     }
 }
 
@@ -21,4 +23,11 @@ window.onload = function() {
     if (String(location).includes("dev")) {
         document.getElementsByClassName('dev-about')[0].click();
     }
+    var linkedAlert = setInterval(function() {
+        if (document.getElementsByClassName('LI-view-profile').length != 0) {
+            document.getElementsByClassName('linkedin-script-alert')[0].remove();
+            clearInterval(linkedAlert);
+        }
+    }, 1000);
+    setTimeout(function() { clearInterval(linkedAlert); }, 5000);
 }
