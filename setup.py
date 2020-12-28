c8f6da266dddf898cf2e6f3fdfab29637ab6e680
BlackTiger145 <62836633+BlackTiger145@users.noreply.github.com>
2020-12-28 17:53:52 +0200

    add author

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='flask web project',
+    author="Itai Dotan",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
